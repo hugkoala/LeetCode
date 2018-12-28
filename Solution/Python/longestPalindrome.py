c25c866aa65c0d46185d331021b8c90ecdecf7b5
hugkoala <bmwbmwtruck1@gmail.com>
2018-12-29 02:34:43 +0900

    Update longestPalindrome.py

--- Solution/Python/longestPalindrome.py
+++ Solution/Python/longestPalindrome.py
@@ -4,15 +4,58 @@ class Solution:
         :type s: str
         :rtype: str
         """
-
-
-    def checkPalindrome(self, s, i, j, idx):
-        if idx > len(s) or idx < 0:
+        if len(s) == 0:
             return ''
-        elif s[i] == s[j]:
-            if i + 1 < len(s) and j - 1 > 0:
-                return checkPalindrome(s, i + 1, j - 1, idx)
+        elif len(s) == 1:
+            return s
+        elif len(s) == 2:
+            if s[0] == s[1]:
+                return s
             else:
-                return s[j : i + 1]
+                return s[0]
         else:
-            str1 = checkPalindrome(s, i + 1, j + 1, idx + 1)
+            longestPalindrome = ''
+
+            for idx in range(0, len(s)):
+                if idx + 1 < len(s):
+                    isPalindrome = None
+                    i = idx + 1
+                    j = idx
+                    while isPalindrome or isPalindrome == None:
+                        if s[i] == s[j]:
+                            if i + 1 >= len(s) or j - 1 < 0:
+                                isPalindrome = False
+                                if len(longestPalindrome) < i - j + 1:
+                                    longestPalindrome = s[j: i + 1]
+                            else:
+                                isPalindrome = True
+                                i += 1
+                                j -= 1
+                        else:
+                            if isPalindrome:
+                                if len(longestPalindrome) < i - j - 1:
+                                    longestPalindrome = s[j + 1: i]
+                            isPalindrome = False
+                if idx + 1 < len(s) and idx - 1 >= 0:
+                    isPalindrome = None
+                    i = idx + 1
+                    j = idx - 1
+                    while isPalindrome or isPalindrome == None:
+                        if s[i] == s[j]:
+                            if i + 1 >= len(s) or j - 1 < 0:
+                                isPalindrome = False
+                                if len(longestPalindrome) < i - j + 1:
+                                    longestPalindrome = s[j: i + 1]
+                            else:
+                                isPalindrome = True
+                                i += 1
+                                j -= 1
+                        else:
+                            if isPalindrome:
+                                if len(longestPalindrome) < i - j - 1:
+                                    longestPalindrome = s[j + 1: i]
+                            isPalindrome = False
+            if longestPalindrome == '':
+                return s[0]
+            else:
+                return longestPalindrome
