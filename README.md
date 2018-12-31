907d6aff23ae8f3debc7cd51bb4b09f479bd4cd7
hugkoala <bmwbmwtruck1@gmail.com>
2019-01-01 00:45:47 +0900

    generateParenthesis Accepted

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ LeetCode
 |3|[Longest Palindromic Substring](https://oj.leetcode.com/problems/longest-palindromic-substring/)| [Python](./Solution/Python/longestPalindrome.py)|Medium|Accepted|
 |12|[Integer to Roman](https://oj.leetcode.com/problems/integer-to-roman/)| [Python](./Solution/Python/intToRoman.py)|Medium|Time Limit Exceeded|
 |15|[3Sum](https://oj.leetcode.com/problems/3sum/)| [Python](./Solution/Python/threeSum.py)|Medium|Accepted|
-|22|[Generate Parentheses](https://oj.leetcode.com/problems/generate-parentheses/)| [Python](./Solution/Python/generateParenthesis.py)|Medium||
+|22|[Generate Parentheses](https://oj.leetcode.com/problems/generate-parentheses/)| [Python](./Solution/Python/generateParenthesis.py)|Medium|Accepted|
 
 
 ### LeetCode Shell
--- Solution/Python/generateParenthesis.py
+++ Solution/Python/generateParenthesis.py
@@ -1,6 +1,53 @@
 class Solution:
+
     def generateParenthesis(self, n):
         """
         :type n: int
         :rtype: List[str]
         """
+
+        results = list()
+        return self.recParenthesis(n, '', 0, 0)
+
+
+    def recParenthesis(self, n, result, left, right):
+
+        if left > n or right > n:
+            return ''
+        elif left + right == n * 2:
+            # check Parenthesis
+            result_str = self.checkParenthesis(result)
+            if result_str == '':
+                return
+            else:
+                return [result_str, ]
+        else:
+            left_list = self.recParenthesis(n, result + '(', left + 1, right)
+            right_list = self.recParenthesis(n, result + ')', left, right + 1)
+            results = list()
+
+            if left_list:
+                results = results + left_list
+            if right_list:
+                results = results + right_list
+            return results
+
+    def checkParenthesis(self, result):
+
+        parentMap = {'(': ')'}
+        stack = list()
+        stack.append('#')
+
+        idx = 0
+        while not len(stack) == 0:
+            char = result[idx]
+
+            if char == '(':
+                stack.append(char)
+            elif char == ')':
+                if char != parentMap.get(stack.pop()):
+                    return ''
+            idx += 1
+
+            if idx >= len(result):
+                return result
