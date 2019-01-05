4af93d46f3311f9825596788804b060816a96fbc
hugkoala <bmwbmwtruck1@gmail.com>
2019-01-06 00:34:10 +0900

    isValidSudoku.py Accepted

--- README.md
+++ README.md
@@ -15,8 +15,8 @@ LeetCode
 |19|[Remove Nth Node From End of List](https://oj.leetcode.com/problems/remove-nth-node-from-end-of-list/)| [Python](./Solution/Python/removeNthFromEnd.py)|Medium|Accepted|
 |22|[Generate Parentheses](https://oj.leetcode.com/problems/generate-parentheses/)| [Python](./Solution/Python/generateParenthesis.py)|Medium|Accepted|
 |34|[Find First and Last Position of Element in Sorted Array](https://oj.leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/)| [Python](./Solution/Python/searchRange.py)|Medium|Accepted|
-|36|[Valid Sudoku](https://oj.leetcode.com/problems/valid-sudoku/)| [Python](./Solution/Python/isValidSudoku.py)|Medium||
-|38|[Count and Say](https://oj.leetcode.com/problems/fcount-and-say/)| [Python](./Solution/Python/countAndSay.py)|Easy|Accepted|
+|36|[Valid Sudoku](https://oj.leetcode.com/problems/valid-sudoku/)| [Python](./Solution/Python/isValidSudoku.py)|Medium|Accepted|
+|38|[Count and Say](https://oj.leetcode.com/problems/count-and-say/)| [Python](./Solution/Python/countAndSay.py)|Easy|Accepted|
 |41|[First Missing Positive](https://oj.leetcode.com/problems/first-missing-positive/)| [Python](./Solution/Python/firstMissingPositive.py)|Hard|Accepted|
 
 
--- Solution/Python/isValidSudoku.py
+++ Solution/Python/isValidSudoku.py
@@ -4,3 +4,48 @@ class Solution:
         :type board: List[List[str]]
         :rtype: bool
         """
+        for i in range(0, 9):
+            if self.rowValid(board, i):
+                if self.columnValid(board, i):
+                    if not self.quarterValid(board, i):
+                        return False
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return True
+
+
+    def quarterValid(self, board, n):
+        centerX = 1 + (n % 3) * 3
+        centerY = 1 + (n // 3) * 3
+        subBoard = [board[centerX - 1][centerY - 1], board[centerX][centerY - 1], board[centerX + 1][centerY - 1]
+            , board[centerX - 1][centerY], board[centerX][centerY], board[centerX + 1][centerY]
+            , board[centerX - 1][centerY + 1], board[centerX][centerY + 1], board[centerX + 1][centerY + 1]]
+
+        for i in range(0, 9):
+            if subBoard[i] != '.' and subBoard[i] in subBoard[i + 1 : 9]:
+                return False
+        else:
+            return True
+
+    def rowValid(self, board, n):
+        subBoard = [board[n][0], board[n][1], board[n][2]
+            , board[n][3], board[n][4], board[n][5]
+            , board[n][6], board[n][7], board[n][8]]
+        for i in range(0, 9):
+            if subBoard[i] != '.' and subBoard[i] in subBoard[i + 1 : 9]:
+                return False
+        else:
+            return True
+
+    def columnValid(self, board, n):
+        subBoard = [board[0][n], board[1][n], board[2][n]
+            , board[3][n], board[4][n], board[5][n]
+            , board[6][n], board[7][n], board[8][n]]
+        for i in range(0, 9):
+            if subBoard[i] != '.' and subBoard[i] in subBoard[i + 1 : 9]:
+                return False
+        else:
+            return True
