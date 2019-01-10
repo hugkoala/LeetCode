925cd2beb3773a60e3d6aeb8b003cbe1073061ec
hugkoala <bmwbmwtruck1@gmail.com>
2019-01-11 01:03:30 +0900

    search.py Accepted

--- Solution/Python/search.py
+++ Solution/Python/search.py
@@ -5,3 +5,30 @@ class Solution:
         :type target: int
         :rtype: int
         """
+        if len(nums) == 0:
+            return -1
+
+        left = 0
+        right = len(nums) - 1
+
+        while True:
+            if nums[left] == target:
+                return left
+            if nums[right] == target:
+                return right
+
+            middle = (left + right) // 2
+            if middle == right or middle == left:
+                return -1
+            if nums[middle] == target:
+                return middle
+            if nums[middle] > nums[right]:
+                if nums[left] <= target and target <= nums[middle]:
+                    right = middle
+                else:
+                    left = middle + 1
+            else:
+                if nums[middle] <= target and target <= nums[right]:
+                    left = middle
+                else:
+                    right = middle - 1
