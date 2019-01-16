9737e86dba4b48281b225191d043abd4e396c3c0
hugkoala <bmwbmwtruck1@gmail.com>
2019-01-17 00:36:29 +0900

    Update canJump.py

--- Solution/Python/canJump.py
+++ Solution/Python/canJump.py
@@ -4,3 +4,8 @@ class Solution:
         :type nums: List[int]
         :rtype: bool
         """
+        for idx, num in enumerate(nums):
+            if idx + num > len(nums) - 1:
+                return True
+        else:
+            return False
