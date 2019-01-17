e9d72cbe3d0fa6578c17f5e00040195c8151aad0
hugkoala <bmwbmwtruck1@gmail.com>
2019-01-17 23:14:32 +0900

    canJump.py Accepted

--- Solution/Python/canJump.py
+++ Solution/Python/canJump.py
@@ -1,11 +1,18 @@
-class Solution:
+class Solution(object):
     def canJump(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
+        max_len = 0
+        
         for idx, num in enumerate(nums):
-            if idx + num > len(nums) - 1:
+            if max_len < idx:
+                return False
+            if idx + num > max_len:
+                max_len = idx + num
+                
+            if max_len >= len(nums) - 1:
                 return True
         else:
             return False
